d7ce90b9ffe5fac6f81a7df81338949295d49568
WilliamSanchez <sanchezwill596@gmail.com>
2025-05-11 17:15:12 -0500

    FIFTH COMMIT

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 function modalVideo() {
-
+    
     let selector = document.getElementById("modal-1");
 
     selector.classList.toggle("active");
-
+    
 }
 
-function menuMobile() {
+function menuMobile(){
     let selector = document.getElementById("cont-menu-mobile");
+
     selector.classList.toggle("active");
 }
+
+function modalVideoAbout() {
+    
+    let selector = document.getElementById("modal-2");
+
+    selector.classList.toggle("active");
+    
+}
+
+document.getElementById('discoverBtn').addEventListener('click', function() {
+    var moreCars = document.getElementById('more-cars');
+    
+    // Alternar entre mostrar y ocultar los vehículos adicionales
+    if (moreCars.style.display === 'none' || moreCars.style.display === '') {
+        moreCars.style.display = 'flex'; // Cambiar a 'flex' o 'block' según el diseño
+        AOS.refresh(); // Refresca AOS para las animaciones
+    } else {
+        moreCars.style.display = 'none';
+    }
+});
+
+
+
+
